6a31ec72d68eb0cfc84d12bf6f8d3ef998de43d5
rafaelfrancisconogueira <rafaelfrancisconogueira@gmail.com>
2021-08-16 09:44:40 -0300

    tete

--- src/cursosjava/classes/Aluno.java
+++ src/cursosjava/classes/Aluno.java
@@ -12,6 +12,10 @@ public class Aluno {
 	String dataMatricula;
 	String nomeEscola;
 	String serieMatriculado;
+	private double notaAluno01;
+	private double notaAluno02;
+	private double notaAluno03;
+	private double notaAluno04;
 	
 	public Aluno(String nomePadrao,int idadePadrao) {
 		nome = nomePadrao;
@@ -95,4 +99,47 @@ public class Aluno {
 		this.serieMatriculado = serieMatriculado;
 	}
 
+	public double getNotaAluno01() {
+		return notaAluno01;
+	}
+
+	public void setNotaAluno01(double notaAluno01) {
+		this.notaAluno01 = notaAluno01;
+	}
+
+	public double getNotaAluno02() {
+		return notaAluno02;
+	}
+
+	public void setNotaAluno02(double notaAluno02) {
+		this.notaAluno02 = notaAluno02;
+	}
+
+	public double getNotaAluno03() {
+		return notaAluno03;
+	}
+
+	public void setNotaAluno03(double notaAluno03) {
+		this.notaAluno03 = notaAluno03;
+	}
+
+	public double getNotaAluno04() {
+		return notaAluno04;
+	}
+
+	public void setNotaAluno04(double notaAluno04) {
+		this.notaAluno04 = notaAluno04;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+public double getMediaNota() {
+	return (notaAluno01 + notaAluno02 + notaAluno03 + notaAluno04)/4;
 }
+}
+
--- src/orientacaoAobjeto/PrimeiroObjeto.java
+++ src/orientacaoAobjeto/PrimeiroObjeto.java
@@ -6,6 +6,8 @@ public class PrimeiroObjeto {
 
 	public static void main(String[] args) {
 		
+		// TESTE
+		
 		Aluno aluno01 = new Aluno("João",50);
 		Aluno aluno02 = new Aluno("Fabricio",10);
 		
@@ -18,10 +20,15 @@ public class PrimeiroObjeto {
 		aluno01.setNomePai("Joaquin");
 		aluno01.setSerieMatriculado("777.8888.999");
 		
+		aluno01.setNotaAluno01(60);
+		aluno01.setNotaAluno02(50);
+		aluno01.setNotaAluno03(80);
+		aluno01.setNotaAluno04(40);
+		
+		System.out.println("A media do Aluno 01 e de " + aluno01.getMediaNota());
 		
 		
 
-		System.out.println(aluno01.getNomeResponsavel());
 		
 		
 
